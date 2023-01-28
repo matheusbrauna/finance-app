941f9876e9656a58bef951cd9ba6cb2826e9a799
matheusbrauna <mattsilvahenrique102@gmail.com>
2023-01-27 21:13:01 -0300

    fix: fix layout shift when loads the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,32 @@
+import { useSession } from 'next-auth/react'
 import { AllCards } from '../components/CategoryCard/AllCards'
 import { Charts } from '../components/Charts/Charts'
 import { Header } from '../components/Header/Header'
 import { Transactions } from '../components/Transactions/Transactions'
 import styles from '../styles/initial.module.scss'
-// eslint-disable-next-line camelcase
-import { unstable_getServerSession } from 'next-auth'
-import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const { status } = useSession()
+  const { push } = useRouter()
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      push('/login')
+    }
+  }, [push, status])
+
   return (
     <>
-      <main className={styles.main}>
-        <Header />
-        <AllCards />
-        <Charts />
-        <Transactions />
-      </main>
+      {status === 'authenticated' && (
+        <main className={styles.main}>
+          <Header />
+          <AllCards />
+          <Charts />
+          <Transactions />
+        </main>
+      )}
     </>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const session = await unstable_getServerSession(req, res, {})
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    }
-  }
-
-  return {
-    props: {
-      session,
-    },
-  }
 }
