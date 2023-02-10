1bfb148e25b1cbe58f6550a2142df40df8b15cf9
matheusbrauna <mattsilvahenrique102@gmail.com>
2023-02-10 10:32:52 -0300

    fix: fix transferAmount component

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
   images: { domains: ['lh3.googleusercontent.com'] },
   async redirects() {
     return [
