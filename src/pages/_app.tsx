0c11734aa8492ba2ebf084b980d50bf49d1e61eb
Matheus Braúna <mattsilvahenrique102@gmail.com>
2023-04-11 12:01:42 -0300

    feat: changing stack

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { type AppType } from 'next/app'
+
+import { api } from '~/utils/api'
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default api.withTRPC(MyApp)
