f49825c9955a879781306980be3e1fae393fa217
Matheus Braúna <matheusbrauna.contato@gmail.com>
2024-05-21 11:03:46 -0300

    feat(dashboard): add mobile navigation

--- src/components/layouts/navigation.tsx
+++ src/components/layouts/navigation.tsx
@@ -1,7 +1,12 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
+import { useMedia } from 'react-use'
+import { useState } from 'react'
 import { NavButton } from './nav-button'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
+import { Menu } from 'lucide-react'
 
 const routes = [
   {
@@ -27,7 +32,46 @@ const routes = [
 ]
 
 export function Navigation() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const router = useRouter()
   const pathname = usePathname()
+  const isMobile = useMedia('(max-width: 1024px)', false)
+
+  const onClick = (href: string) => {
+    router.push(href)
+    setIsOpen(false)
+  }
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant="outline"
+            size="sm"
+            className="border-none bg-white/10 font-normal text-white outline-none transition hover:bg-white/20 hover:text-white focus:bg-white/30 focus-visible:ring-transparent focus-visible:ring-offset-0"
+          >
+            <Menu className="size-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="px-2">
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {routes.map((route) => (
+              <Button
+                key={route.href}
+                variant={route.href === pathname ? 'secondary' : 'ghost'}
+                onClick={() => onClick(route.href)}
+                className="w-full justify-start"
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    )
+  }
 
   return (
     <nav className="hidden items-center gap-x-2 overflow-x-auto lg:flex">
