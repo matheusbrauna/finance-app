05b1d808022ad0b9e746ec321e5c0cfbbadd3451
Matheus Braúna <matheusbrauna.contato@gmail.com>
2024-05-29 11:50:01 -0300

    feat(api): add get account by id route

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -25,6 +25,42 @@ const app = new Hono()
 
     return c.json({ data })
   })
+  .get(
+    '/:id',
+    clerkMiddleware(),
+    zValidator(
+      'param',
+      z.object({
+        id: z.string().cuid2().optional(),
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c)
+      const { id } = c.req.valid('param')
+
+      if (!id) {
+        return c.json({ error: 'Missing id' }, 400)
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: 'unauthorized' }, 401)
+      }
+
+      const [data] = await db
+        .select({
+          id: accounts.id,
+          name: accounts.name,
+        })
+        .from(accounts)
+        .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)))
+
+      if (!data) {
+        return c.json({ error: 'Not found' }, 404)
+      }
+
+      return c.json({ data })
+    },
+  )
   .post(
     '/',
     clerkMiddleware(),
