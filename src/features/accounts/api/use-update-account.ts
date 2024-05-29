245e1b5da10d66a3091fe59d3f943f089e136c10
Matheus Braúna <matheusbrauna.contato@gmail.com>
2024-05-29 14:25:59 -0300

    feat(features): add use edit account api hook

---
+++ src/features/accounts/api/use-update-account.ts
@@ -0,0 +1,33 @@
+import { client } from '@/lib/hono'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { InferRequestType, InferResponseType } from 'hono'
+import { toast } from 'sonner'
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[':id']['$patch']
+>
+type RequestType = InferRequestType<
+  (typeof client.api.accounts)[':id']['$patch']
+>['json']
+
+export const useEditAccount = (id?: string) => {
+  const queryClient = useQueryClient()
+  return useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const res = await client.api.accounts[':id'].$patch({
+        json,
+        param: { id },
+      })
+      return await res.json()
+    },
+    onSuccess: () => {
+      toast.success('Account updated')
+      queryClient.invalidateQueries({ queryKey: ['account', { id }] })
+      queryClient.invalidateQueries({ queryKey: ['accounts'] })
+      // TODO: Invalidate summary and transactions
+    },
+    onError: () => {
+      toast.error('Failed to edit account')
+    },
+  })
+}
