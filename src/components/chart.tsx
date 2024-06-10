fc67e685fe382fa9772a877c9b9d2684d771ecb5
Matheus Braúna <matheusbrauna.contato@gmail.com>
2024-06-10 17:56:05 -0300

    feat(dashboard): add data charts

---
+++ src/components/chart.tsx
@@ -0,0 +1,100 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  AreaChart,
+  BarChart3,
+  FileSearch,
+  LineChart,
+  Loader2,
+} from 'lucide-react'
+import { AreaVariant } from '@/components/area-variant'
+import { BarVariant } from '@/components/bar-variant'
+import { LineVariant } from '@/components/line-variant'
+import { useState } from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from '@/components/ui/select'
+import { SelectValue } from '@radix-ui/react-select'
+import { Skeleton } from '@/components/ui/skeleton'
+
+type Props = {
+  data?: Array<{ date: string; income: number; expenses: number }>
+}
+
+export function Chart({ data = [] }: Props) {
+  const [chartType, setChartType] = useState('area')
+
+  function onTypeChange(type: string) {
+    // TODO: Add paywall
+
+    setChartType(type)
+  }
+
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex justify-between space-y-2 lg:flex-row lg:items-center lg:space-y-0">
+        <CardTitle className="line-clamp-1 text-xl">Transactions</CardTitle>
+        <Select defaultValue={chartType} onValueChange={onTypeChange}>
+          <SelectTrigger className="h-9 rounded-md px-3 lg:w-auto">
+            <SelectValue placeholder="Chart type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="area">
+              <div className="flex items-center">
+                <AreaChart className="mr-2 size-4 shrink-0" />
+                <p className="line-clamp-1">Area chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="line">
+              <div className="flex items-center">
+                <LineChart className="mr-2 size-4 shrink-0" />
+                <p className="line-clamp-1">Line chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="bar">
+              <div className="flex items-center">
+                <BarChart3 className="mr-2 size-4 shrink-0" />
+                <p className="line-clamp-1">Bar chart</p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent>
+        {data.length === 0 ? (
+          <div className="flex h-[350px] w-full flex-col items-center justify-center gap-y-4">
+            <FileSearch className="size-6 text-muted-foreground" />
+            <p className="text-sm text-muted-foreground">
+              No data for this period.
+            </p>
+          </div>
+        ) : (
+          <>
+            {chartType === 'line' && <LineVariant data={data} />}
+            {chartType === 'area' && <AreaVariant data={data} />}
+            {chartType === 'bar' && <BarVariant data={data} />}
+          </>
+        )}
+      </CardContent>
+    </Card>
+  )
+}
+
+export const ChartLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex justify-between space-y-2 lg:flex-row lg:items-center lg:space-y-0">
+        <Skeleton className="h-8 w-48" />
+        <Skeleton className="h-8 w-full lg:w-[120px]" />
+      </CardHeader>
+
+      <CardContent>
+        <div className="flex h-[350px] w-full items-center justify-center">
+          <Loader2 className="size-6 animate-spin text-slate-300" />
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/components/data-charts.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useGetSummary } from '@/features/summary/api/use-get-summary'
+import { Chart, ChartLoading } from '@/components/chart'
+
+export function DataCharts() {
+  const { data, isLoading } = useGetSummary()
+
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-6">
+        <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+          <ChartLoading />
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-6">
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+        <Chart data={data?.days} />
+      </div>
+    </div>
+  )
+}
